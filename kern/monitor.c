ef7790c77427d7089ddc0c61ddca6b9226020dbf
ryanmercado6 <32846563+ryanmercado6@users.noreply.github.com>
2022-12-09 22:07:05 -0800

    Adding lab3 files

--- kern/monitor.c
+++ kern/monitor.c
@@ -10,6 +10,7 @@
 #include <kern/console.h>
 #include <kern/monitor.h>
 #include <kern/kdebug.h>
+#include <kern/trap.h>
 
 #define CMDBUF_SIZE	80	// enough for one VGA text line
 
@@ -22,8 +23,6 @@ struct Command {
 };
 
 static struct Command commands[] = {
-	//adding backtrace commands
-	{"backtrace" , "Stack backtrace", mon_backtrace },
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
 };
@@ -59,30 +58,8 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-    int hasinfo;
-    uint32_t eip, ebp;
-    register int i;
-    struct Eipdebuginfo info;
-    cprintf("Stack backtraces");
-    cprintf("\n");
-    ebp = read_ebp();
-    while (ebp > 1) {
-        // printing traces using ebp to find eip and args
-        cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", 
-                ebp, *(uint32_t *)(ebp + 4), *(uint32_t *)(ebp + 8), 		*(uint32_t *)(ebp + 12),
-                *(uint32_t *)(ebp + 16), *(uint32_t *)(ebp + 20), *(uint32_t *)(ebp + 24));
-       //assigning eip
-       eip = *(uint32_t *) ((uint32_t *) ebp +1);
-       hasinfo = debuginfo_eip(eip, &info);
-       cprintf("          %s:%d: ", info.eip_file, info.eip_line);
-       for(i=0; i<info.eip_fn_namelen; i++)
-            cprintf("%c", *(info.eip_fn_name+i));
-       cprintf("+%d\n", eip - info.eip_fn_addr);
-       //host ebp
-       ebp = *(uint32_t *)ebp;
-    }
-    return 0;
-
+	// Your code here.
+	return 0;
 }
 
 
@@ -139,6 +116,8 @@ monitor(struct Trapframe *tf)
 	cprintf("Welcome to the JOS kernel monitor!\n");
 	cprintf("Type 'help' for a list of commands.\n");
 
+	if (tf != NULL)
+		print_trapframe(tf);
 
 	while (1) {
 		buf = readline("K> ");
